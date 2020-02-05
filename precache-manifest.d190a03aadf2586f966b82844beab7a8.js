8a870fc04aafa5b88d65d33ce68c54d8e3ea7396
S1EpoY <slepoy1992@gmail.com>
2020-02-05 20:46:48 +0200

    Updates

---
+++ precache-manifest.d190a03aadf2586f966b82844beab7a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d78f09519c4d1f407b9b9c0b60ccfa7",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "f2f4ee78a935f9eeaa76",
+    "url": "/goit-react-hw-01-components/static/css/main.eaa2e772.chunk.css"
+  },
+  {
+    "revision": "34bdfe2281bff29a5694",
+    "url": "/goit-react-hw-01-components/static/js/2.dab32b2d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/goit-react-hw-01-components/static/js/2.dab32b2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2f4ee78a935f9eeaa76",
+    "url": "/goit-react-hw-01-components/static/js/main.18a4ae7b.chunk.js"
+  },
+  {
+    "revision": "5529903743abc23e6f33",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.5c5371af.js"
+  }
+]);
